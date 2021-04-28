69868dde8b4fc85bd5c92bd501065ff6cbcf842e
Ahmed Aziz <ahmedshawky23@gmail.com>
2021-04-28 19:50:50 -0400

    finished exercise 2.2 and 2.3

--- part2/course info/src/App.js
+++ part2/course info/src/App.js
@@ -7,7 +7,7 @@ const Header = ({ course }) => {
 }
 
 const Total = ({ course }) => {
-  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  const sum =  course.parts.reduce( (s,p) => s + p.exercises , 0)
   return(
     <p>Number of exercises {sum}</p>
   ) 
@@ -34,6 +34,7 @@ const Course =({ course }) => {
     <div>
     <Header course={course} />
     <Content course={course} />
+    <Total course={course} />
     </div>
   )
 }
