e2f31674a22e116c42803bc08fac0ced0bdb4c44
Ahmed Aziz <ahmedshawky23@gmail.com>
2021-05-02 21:31:36 -0400

    finished exercise 2.15

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -25,9 +25,14 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
     } else {
       const newPerson = { name: newName, number: newNumber}
-      setPersons(persons.concat(newPerson))
-      setNewName('')
-      setNewNumber('')
+      axios
+      .post('http://localhost:3001/persons', newPerson)
+      .then(reposnse => {
+        setPersons(persons.concat(reposnse.data))
+        setNewName('')
+        setNewNumber('')
+
+      })
     }
   }
 
