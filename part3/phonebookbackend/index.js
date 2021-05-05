2e439da684247aa7b9bb931db2701335e7deafcd
Ahmed Aziz <ahmedshawky23@gmail.com>
2021-05-04 21:48:35 -0400

    finished exercise 3.5

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 persons = [
 	{
 		name: 'Arto Hellas',
@@ -50,6 +52,15 @@ app.delete('/api/persons/:id', (request, response) => {
 	persons = persons.filter((person) => person.id !== id)
 
 	response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+	const person = request.body
+	person.id = Math.floor(Math.random() * 100) + 4
+
+	persons = persons.concat(person)
+
+	response.json(person)
 })
 
 const PORT = 3001
