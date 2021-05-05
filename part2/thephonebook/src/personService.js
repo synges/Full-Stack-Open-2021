22225526abe01147a7f63327172a9a35c80af671
Ahmed Aziz <ahmedshawky23@gmail.com>
2021-05-05 19:41:13 -0400

    finished exercise 3.9

--- part2/thephonebook/src/personService.js
+++ part2/thephonebook/src/personService.js
@@ -1,24 +1,24 @@
-import axios from 'axios';
-const baseUrl = 'http://localhost:3001/persons';
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
-	const request = axios.get(baseUrl);
-	return request.then((response) => response.data);
-};
+	const request = axios.get(baseUrl)
+	return request.then((response) => response.data)
+}
 
 const create = (newPerson) => {
-	const request = axios.post(baseUrl, newPerson);
-	return request.then((reponse) => reponse.data);
-};
+	const request = axios.post(baseUrl, newPerson)
+	return request.then((reponse) => reponse.data)
+}
 
 const deletePerson = (person) => {
-	const request = axios.delete(`${baseUrl}/${person.id}`);
-	return request.then((response) => response.status);
-};
+	const request = axios.delete(`${baseUrl}/${person.id}`)
+	return request.then((response) => response.status)
+}
 
 const update = (person, updatedPerson) => {
-	const request = axios.put(`${baseUrl}/${person.id}`, updatedPerson);
-	return request.then((response) => response.data);
-};
+	const request = axios.put(`${baseUrl}/${person.id}`, updatedPerson)
+	return request.then((response) => response.data)
+}
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll, create, deletePerson, update };
+export default { getAll, create, deletePerson, update }
