2227a65715268493659a81eaefbe6de964b158f4
Ahmed Aziz <ahmedshawky23@gmail.com>
2021-05-16 18:42:26 -0400

    finished exercise 4.3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -4,11 +4,24 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
 	const reducer = (sum, item) => sum + item.likes;
-
 	return blogs.reduce(reducer, 0);
+};
+
+const favoriteBlog = (blogs) => {
+	const reducer = (favorite, item) => {
+		if (favorite.likes < item.likes) {
+			favorite.title = item.title;
+			favorite.author = item.author;
+			favorite.likes = item.likes;
+		}
+		return favorite;
+	};
+
+	return blogs.reduce(reducer, { likes: 0 });
 };
 
 module.exports = {
 	dummy,
 	totalLikes,
+	favoriteBlog,
 };
