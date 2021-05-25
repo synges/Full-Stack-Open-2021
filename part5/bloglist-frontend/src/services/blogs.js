0a93ec5867bbe89b2367bc71fe80b6b540f72ed1
Ahmed Aziz <ahmedshawky23@gmail.com>
2021-05-25 19:35:40 -0400

    finished exercise 5.1

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,16 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+let token = null
+
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`
 }
 
-export default { getAll }
+const getAll = () => {
+  const config = { headers: { Authorization: token } }
+  const request = axios.get(baseUrl, config)
+  return request.then((response) => response.data)
+}
+
+export default { getAll, setToken }
