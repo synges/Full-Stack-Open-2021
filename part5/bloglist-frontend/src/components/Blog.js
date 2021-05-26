87d445a594a6e9fa35d895b2f05476d31ab1fd6c
Ahmed Aziz <ahmedshawky23@gmail.com>
2021-05-26 15:53:13 -0400

    finished exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,39 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleVisibility = () => {
+    setShowDetails(!showDetails)
+  }
+  if (showDetails) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>hide</button>
+        <br />
+        {blog.url}
+        <br />
+        likes {blog.likes}
+        <button>like</button>
+        <br />
+        {blog.user.name}
+      </div>
+    )
+  } else {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+    )
+  }
+}
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5,
+}
 
 export default Blog
