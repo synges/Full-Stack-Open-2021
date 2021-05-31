a91af2051842e39c080c31f226d3bc955a76736a
Ahmed Aziz <ahmedshawky23@gmail.com>
2021-05-31 14:34:21 -0400

    finished exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,28 +1,46 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders content', () => {
-  const blog = {
-    title: 'Component testing is done with react-testing-library',
-    likes: 10,
-    author: 'Ahmed Aziz',
-    url: 'www.expample.com',
-    user: { name: 'Ahmed', username: 'test username' },
-  }
+describe('<Blog />', () => {
+  let component
 
-  const component = render(<Blog blog={blog} />)
+  beforeEach(() => {
+    const blog = {
+      title: 'Component testing is done with react-testing-library',
+      likes: 10,
+      author: 'Ahmed Aziz',
+      url: 'www.expample.com',
+      user: { name: 'Ahmed', username: 'test username' },
+    }
+    const loggedUser = {
+      username: 'test username',
+    }
 
-  expect(component.container).toHaveTextContent(
-    'Component testing is done with react-testing-library'
-  )
+    component = render(<Blog blog={blog} loggedUser={loggedUser} />)
+  })
 
-  const basicDiv = component.container.querySelector('.blog')
-  //   const detaildDiv = component.container.querySelector('.blog')
-  expect(basicDiv).toHaveTextContent(
-    'Component testing is done with react-testing-library'
-  )
-  expect(basicDiv).not.toHaveTextContent('www.expample.com')
-  expect(basicDiv).not.toHaveTextContent('likes')
+  test('renders content with basic info', () => {
+    expect(component.container).toHaveTextContent(
+      'Component testing is done with react-testing-library'
+    )
+
+    const basicDiv = component.container.querySelector('.blog')
+    //   const detaildDiv = component.container.querySelector('.blog')
+    expect(basicDiv).toHaveTextContent(
+      'Component testing is done with react-testing-library'
+    )
+    expect(basicDiv).not.toHaveTextContent('www.expample.com')
+    expect(basicDiv).not.toHaveTextContent('likes')
+  })
+
+  test('detailed info is visible with button press', () => {
+    expect(component.container).not.toHaveTextContent('www.expample.com')
+
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    expect(component.container).toHaveTextContent('www.expample.com')
+  })
 })
